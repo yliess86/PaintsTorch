9b9a6ffa4e9a316932b7c9b27806895d2efa4192
yliess86 <hatiyliess86@gmail.com>
2019-06-04 23:47:01 +0200

    Fixed donwload tar.xz

--- download.sh
+++ download.sh
@@ -1,11 +1,15 @@
 ##!/usr/bin/env bash
 
-echo "[download.sh] Creating 'res/model' folder"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "$GREEN [download.sh] Creating 'res/model' folder $NC"
 if [ ! -d res/model ]; then
   mkdir res/model
 fi
 
-echo "[download.sh] Downloading models"
+echo -e "$GREEN [download.sh] Downloading models $NC"
 filename="res/model/models.tar.xz"
 file_id="1bPj7WxT9myNkoIH_vdCWTi8ZtzFqyf8R"
 query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
@@ -15,6 +19,6 @@ url="https://drive.google.com$query"
 curl -b ./cookie.txt -L -o ${filename} $url
 tar -xf ${filename} -C res/model
 
-echo "[download.sh] Removing temp files"
+echo -e "$GREEN [download.sh] Removing temp files $NC"
 rm ${filename}
 rm ./cookie.txt
