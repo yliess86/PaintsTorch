55a6d77ebda116d6e18f924dfca96d1995482a28
yliess86 <hatiyliess86@gmail.com>
2019-06-04 23:40:08 +0200

    Fixed donwload tar.xz

--- download.sh
+++ download.sh
@@ -6,14 +6,14 @@ if [ ! -d res/model ]; then
 fi
 
 echo "[download.sh] Downloading models"
-filename="res/model/models.tar.gz"
+filename="res/model/models.tar.xz"
 file_id="1bPj7WxT9myNkoIH_vdCWTi8ZtzFqyf8R"
 query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
 | perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
 | sed -e 's/amp;//g' | sed -n 2p`
 url="https://drive.google.com$query"
 curl -b ./cookie.txt -L -o ${filename} $url
-tar -xvf ${filename} -C res/model
+tar -xf ${filename} -C res/model
 
 echo "Removing temp files"
 rm ${filename}
